e52c9d8c32923b967d95a143275ea7926659b43a
Alexander Vyzhimov <vizimov@gmail.com>
2023-02-23 20:49:11 +0200

    fixed-errors

---
+++ src/js/gallery-API.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+const API_KEY = '33863715-df25260fa40bd11fad8b98be3';
+const END_POINT = 'https://pixabay.com/api/';
+const DEFAULT_PARAMS =
+  'image_type=photo&orientation=horizontal&safesearch=true';
+
+export default class GalleryApiService {
+  constructor() {
+    this.page = 1;
+    this.searchQuery = '';
+    this.elemCount = 0;
+    this.totalHits = 0;
+  }
+
+  async getGallery() {
+    const URL = `${END_POINT}?key=${API_KEY}&q=${this.searchQuery}&page=${this.page}&${DEFAULT_PARAMS}`;
+
+    try {
+      const response = await axios.get(URL);
+      const { hits, totalHits } = response.data;
+      this.nextPage();
+      return { hits, totalHits };
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  nextPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  setTotalHits(hits) {
+    return (this.totalHits = hits);
+  }
+}
---
+++ src/js/indexloadmore.js
@@ -0,0 +1,107 @@
+import Notiflix from 'notiflix';
+import GalleryApiService from './gallery-API';
+import LoadMoreBtn from './components/load-more-btn';
+import SimpleLightbox from 'simplelightbox';
+
+const input = document.getElementById('search-form');
+const photoGallery = document.querySelector('.gallery');
+const loadMoreBtn = new LoadMoreBtn({ selector: '#loadMore', isHidden: true });
+const galleryApiService = new GalleryApiService();
+
+input.addEventListener('submit', onSubmit);
+loadMoreBtn.button.addEventListener('click', fetchPhotos);
+galleryApiService.getGallery();
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const form = e.currentTarget;
+  const value = form.elements.searchQuery.value.trim();
+  galleryApiService.searchQuery = value;
+
+  clearGallery();
+  galleryApiService.resetPage();
+  loadMoreBtn.show();
+
+  fetchPhotos().finally(() => {
+    form.reset();
+    Notiflix.Notify.success(
+      `Hooray! We found ${galleryApiService.totalHits} images`
+    );
+  });
+
+  gallery.refresh();
+}
+
+function fetchPhotos() {
+  loadMoreBtn.disable();
+  return galleryApiService
+    .getGallery()
+    .then(gallery => {
+      if (gallery.length === 0) {
+        loadMoreBtn.hide();
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+
+      const total = gallery.totalHits;
+      galleryApiService.setTotalHits(total);
+
+      return gallery.hits.reduce(
+        (markup, photo) => createMarkup(photo) + markup,
+        ''
+      );
+    })
+    .then(markup => {
+      appendPhotoToGallery(markup);
+      loadMoreBtn.enable();
+      galleryApiService.elemCount = photoGallery.children.length;
+      if (galleryApiService.elemCount === galleryApiService.totalHits) {
+        Notiflix.Notify.warning(
+          "We're sorry, but you've reached the end of search results."
+        );
+        loadMoreBtn.hide();
+      }
+    });
+}
+
+function createMarkup({
+  webformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  return `
+     <div class="photo-card">
+        <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+        <div class="info">
+            <p class="info-item">
+                <b>Likes</b> <span>${likes}</span>
+            </p>
+            <p class="info-item">
+                <b>Views</b> <span>${views}</span>
+            </p>
+            <p class="info-item">
+                <b>Comments</b> <span>${comments}</span>
+            </p>
+            <p class="info-item">
+                <b>Downloads</b> <span>${downloads}</span>
+            </p>
+        </div>
+    </div>
+    `;
+}
+
+function appendPhotoToGallery(markup) {
+  photoGallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearGallery() {
+  photoGallery.innerHTML = '';
+}
+
+let gallery = new SimpleLightbox('.photo-card a');
